271ce8343a04c96cf242a62d37d38293809b1c58
drudro <drudro@yandex.ru>
2017-12-12 00:56:40 +0200

    21 presentation

--- src/main/Persons.js
+++ src/main/Persons.js
@@ -1,7 +1,11 @@
-
-
-export const Persons = ({users, clickHandler}) => (
-
-            <ul>{users.map(user => <li key={ user.id } onClick={ () => clickHandler(user) }>{user.name}</li>)}</ul>
-    
+export const Persons = ({users, clickHandler, userPosts}) => (
+  <section className="section-users">
+    <ul className="users">{users.map(user => <li key={user.id} onClick={() => clickHandler(user)}>{user.name}</li>)}</ul>
+    <section className="user-posts">
+      <header>
+        <h2>User Posts:</h2>
+      </header>
+      <main>{userPosts.map(post => <article key={post.id}>{post.body}<hr/></article>)}</main>
+    </section>
+  </section>
 );
--- src/main/content/Article.js
+++ src/main/content/Article.js
@@ -1,72 +1,75 @@
-
 import './article.scss';
 import {Counter} from '../Counter';
 import {Persons} from '../Persons';
 import {Numbers} from '../../numbers';
 import {Tabs} from '../../tabs';
-
-
+import {ButtonToggleText} from '../button';
+import {Geo} from '../geo';
 
 const tabs = [
-    {id: 0, title: 'Tab 1', content: 'Some text is here'},
-    {id: 1, title: 'Tab 2', content: 'Another content'},
-    {id: 2, title: 'Tab 1', content: 'Third text'}
-  ];
+    {
+        id: 0,
+        title: 'Tab 1',
+        content: 'Some text is here'
+    }, {
+        id: 1,
+        title: 'Tab 2',
+        content: 'Another content'
+    }, {
+        id: 2,
+        title: 'Tab 1',
+        content: 'Third text'
+    }
+];
 export class Article extends React.Component {
 
     constructor() {
         super();
         this.state = {
             users: [],
-            preloader: false
+            preloader: false,
+            userPosts:[]
         };
     }
     getUsers = () => {
-        this.setState({
-            preloader: true, 
-            users: []
-        });
+        this.setState({preloader: true, users: []});
 
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => {
-                this.setState(
-                    { 
-                        users, 
-                        preloader: false 
-                    }
-                );
+                this.setState({users, preloader: false});
 
                 console.table(users);
             });
     }
 
-    showEmail(user) {
-        alert(
-`${user.name}:
-E-mail: ${user.email}
-Phone: ${user.phone}`
-        );
+    showPosts = (user) => {
+
+        this.setState({userPosts:[]});
+        fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
+            .then(response => response.json())
+            .then(posts => {
+                this.setState({userPosts:posts});
+            });
+
     }
 
     render() {
-        const {users, preloader} = this.state;
+        const {users, preloader, userPosts} = this.state;
         return (
 
             <article className="main__content">
-                <Numbers 
-                    from={7} 
-                    to={14} 
-                    odd
-                />
-
+                <Numbers from={7} to={14} odd/>
+                <ButtonToggleText
+                    toggleText="This button implements 2 tasks: 'toggle button class .active' &amp; 'toggle button text and the related text-box'"/>
+                <Geo/>
                 <Counter/>
 
                 <button
                     onClick={this
                     .getUsers
                     .bind(this)}>Get users</button>
-                <Persons users={users} clickHandler={this.showEmail}/> {preloader && <span>Loading...</span>}
+                <Persons userPosts={userPosts} users={users} clickHandler={this.showPosts}/> {preloader && <span>Loading...</span>}
                 <Tabs tabs={tabs}/>
             </article>
         )
