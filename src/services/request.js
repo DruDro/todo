59daeec804233d4cda9bd53ba0fb9a8c3b89cba7
Dru Dro <drudro.ua@gmail.com>
2018-02-21 20:04:55 +0200

    OKAY

--- src/services/request.js
+++ src/services/request.js
@@ -1,5 +1,5 @@
 
-const db = 'http://localhost:3001/';
+const db = 'http://10.0.2.36:3001/';
 export const request = (url, method='GET', data) => {
     let options = {
         method: method,
@@ -9,5 +9,5 @@ export const request = (url, method='GET', data) => {
           }
     };
     if(data)  options.body = JSON.stringify(data); 
-    return fetch(`${db}${url}`, options).then(res => res.ok ? res.json() : [{title:'loading'}]).catch(err => {throw new Error(err)})
+    return fetch(`${db}${url}`, options).then(res => res.json()).catch(err => {console.log('DB connection is not okay')})
 }
