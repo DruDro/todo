29754ae18ff88daaf5459f8d7f1a661d078dcde3
drudro <drudro@outlook.com>
2018-11-21 19:33:35 +0200

    yeah

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-let jsonServer = require('json-server');
+var jsonServer = require('json-server');
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname,'dist')));
 app.use((req, res, next) => {
 	res.append('Access-Control-Allow-Origin', ['*']);
 	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -11,9 +11,9 @@ app.use((req, res, next) => {
 	next();
 });
 app.use('/api', jsonServer.router('db.json'));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')));
+app.get('*', (req,res) => res.sendFile(path.join(__dirname, 'dist/index.html')));
 const port = 16548;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port, () => console.log(`${server.address().address}:${server.address().port}`));
+server.listen(port, () => console.log(`Running server | Listening to port ${port}`));
